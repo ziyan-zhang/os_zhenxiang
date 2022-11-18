debda37c4aabb36bc981fc62f8984f8542a45b66
zy hasee centos7 <1920548152@qq.com>
2022-11-18 23:35:20 +0800

    keyboard drive: naive

--- c10/kernel/main.c
+++ c10/kernel/main.c
@@ -12,12 +12,12 @@ int main(void) {
    put_str("I am kernel\n");
    init_all();
 
-   thread_start("k_thread_a", 31, k_thread_a, "argA ");
-   thread_start("k_thread_b", 8, k_thread_b, "argB ");
+   // thread_start("k_thread_a", 31, k_thread_a, "argA ");
+   // thread_start("k_thread_b", 8, k_thread_b, "argB ");
 
    intr_enable();	// 打开中断,使时钟中断起作用
-   while(1) {
-      console_put_str("Main ");
+   while(1) {     // 注意while(1)里面可以为空,但是这句话不能被注释掉,否则程序就不知道跑哪里去了(没有人撑着中断)
+      // console_put_str("Main ");
    };
    return 0;
 }
