d4718d24aa9e58816932b8fdd9719efc6d09fb6d
zy hasee centos7 <1920548152@qq.com>
2022-11-23 00:20:42 +0800

    write系统调用与打印16进制

--- c12_SystemCall/lib/user/syscall.h
+++ c12_SystemCall/lib/user/syscall.h
@@ -1,8 +1,10 @@
-#ifndef __USER_SYSCALL_H
-#define __USER_SYSCALL_H
+#ifndef __LIB_USER_SYSCALL_H
+#define __LIB_USER_SYSCALL_H
 #include "stdint.h"
 enum SYSCALL_NR {
-    SYS_GETPID
+    SYS_GETPID,
+    SYS_WRITE
 };
 uint32_t getpid(void);
+uint32_t write(char* str);
 #endif
