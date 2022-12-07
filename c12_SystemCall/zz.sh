8d31a2549c4e30823159f88273720c83c65b9054
ziyanzhang <1920548152@qq.com>
2022-12-07 21:54:31 +0800

    bochs用到的文件放当前文件夹而不是bochs目录；makefile加调试信息

--- c12_SystemCall/zz.sh
+++ c12_SystemCall/zz.sh
@@ -1,9 +1,5 @@
 sudo qemu-system-i386 \
 	-m 1024 -s -S\
 	-serial mon:stdio\
-	-hda /opt/bochs/hd3M.img
-
-	#-hdb /opt/bochs/hd80M.img
-
-#	-drive file=/opt/bochs/hd3M.img,index=0,media=disk,format=raw,if=ide
+	-hda hd3M.img
 #	-drive file=/opt/bochs/hd80M.img,index=1,media=disk,format=raw,if=ide
